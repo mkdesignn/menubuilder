30d4d28b1c4d346965058d940b170816b367887b
mohammad kaab <mohammad.kaab@gmail.com>
2017-06-24 20:48:32 +0430

    change in config, js and style files

--- config/menu.php
+++ config/menu.php
@@ -1,7 +1,21 @@
 <?php
 
 return [
-    "checkbox"=>true,
-    "delete_button"=>true,
-    "direction"=>"rtl"
+    "feature"=> [
+        "checkbox"=>false,
+        "delete_button"=>false,
+    ],
+    "wrapper"=> [
+        "width"=>null,
+        "background"=>"#1A8FCA",
+        "direction"=>"rtl",
+        "font-family"=>"sans-serif"
+    ],
+    "link"=> [
+        "color"=>"#e0e0e0",
+        "border"=>null,
+        "brightness"=>-0.1,
+        "padding"=>null,
+        "hover"=>-0.2
+    ]
 ];
--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,51 @@
-new Vue({
 
+var res = Vue.compile(document.getElementsByClassName("mk_menu_wrapper")[0].outerHTML);
+
+new Vue({
     el:".mk_menu_wrapper",
-    mounted:function(){
-        var menu = document.getElementsByClassName(".mk_menu_wrapper");
-        console.log(menu);
-        this.$compile(menu);
+    render: res.render,
+    staticRenderFns: res.staticRenderFns,
+    data:{
+        current_color:null,
+        style:null
     },
-    method:{
-        openChild:function(){
-            console.log("hello world")
+    methods:{
+        openChild:function(e){
+
+            if( e.currentTarget.parentElement.nextElementSibling.tagName == "UL" ){
+                if( e.currentTarget.parentElement.nextElementSibling.classList.contains("slideDown") ){
+                    e.currentTarget.parentElement.nextElementSibling.classList.remove("slideDown");
+                    e.currentTarget.parentElement.nextElementSibling.classList.add("slideUp");
+                }
+                else{
+                    e.currentTarget.parentElement.nextElementSibling.classList.add("slideDown");
+                    e.currentTarget.parentElement.nextElementSibling.classList.remove("slideUp");
+                }
+            }
+        },
+        mouseOver:function(e){
+
+            if( e.currentTarget.getAttribute("hover") != null ){
+                var hover_color = e.currentTarget.getAttribute("hover"),
+                    style = e.currentTarget.getAttribute("style");
+
+                this.current_color = e.currentTarget.style.background;
+
+                // replace attribute
+                if( this.current_color != "" ){
+                    style = style.replace("background: "+this.current_color+";", "");
+                    this.style = style;
+                }
+
+                e.currentTarget.setAttribute("style", "background:"+hover_color+";"+style)
+            }
+        },
+        mouseOut:function(e){
+
+            if( e.currentTarget.getAttribute("hover") != null ){
+                e.currentTarget.setAttribute("style", "background: "+this.current_color+";"+this.style)
+            }
+
         }
     }
 });
